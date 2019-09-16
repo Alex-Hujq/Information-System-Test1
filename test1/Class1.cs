d0d745518904bd23ff791f94bfc82cefefac9365
94987 <94987@LAPTOP-HP185DJF>
2019-09-16 15:52:36 +0800

    num5

---
+++ test1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        
+        
+            var name = "bugs bunny";
+            var age = 25;
+            var israbbit = true;
+
+            Type nametype = name.GetType();
+            Type agetype = age.GetType();
+            Type israbbittype = israbbit.GetType();
+
+            Console.WriteLine("name is type" + nametype.ToString());
+            Console.WriteLine("age is type" + agetype.ToString());
+            Console.WriteLine("israbbit is type" + israbbit.ToString());
+        
+
+	}
+}
