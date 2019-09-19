f312dcd554318e3cb6a535b04921ebc3f51c06b0
94987 <94987@LAPTOP-HP185DJF>
2019-09-19 12:33:40 +0800

    num8

---
+++ test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type in a string");
+            string input;
+            input = Console.ReadLine();
+            if (input == "")
+            {
+                Console.WriteLine("you typed in an empty string");
+            }
+            else if (input.Length < 5)
+            {
+                Console.WriteLine("the string had less than  5 characters");
+            }
+            else if (input.Length < 10)
+            {
+                Console.WriteLine("the string had at least 5 but less than 10 chart");
+
+            }
+            Console.WriteLine("the string was " + input);
+
+
+        }
+    }
+}
---
+++ test3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static int j = 20;
+        static void Main(string[] args)
+        {
+            int j = 30;
+            Console.WriteLine(j);
+            
+
+            string s1 = "a string";
+            string s2 = s1;
+            Console.WriteLine("s1 is" + s1);
+            Console.WriteLine("s2 is" + s2);
+            s1 = "another string";
+            Console.WriteLine("s1 is now " + s1);
+            Console.WriteLine("s2 is now " + s2);
+
+            string filepath = @"c:\procsharp\first.cs";
+            Console.WriteLine(filepath);
+            string aa = @"'twas brillng
+        did qe";
+            Console.WriteLine(aa);
+
+            int i = 5;
+            bool iszero;
+            if (i == 0)
+            {
+                iszero = true;
+                Console.WriteLine("i is zero");
+            }
+            else
+            {
+                iszero = false;
+                Console.WriteLine("i is non-zero");
+            }
+
+
+
+        }
+    }
+}
